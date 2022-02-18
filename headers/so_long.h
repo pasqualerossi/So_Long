4d091837fb8422fea4e8fc0a603256080f7c9abd
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-02-18 19:38:23 +1030

    Add files via upload

---
+++ headers/so_long.h
@@ -0,0 +1,13 @@
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include <unistd.h>
+# include <fcntl.h>
+# include <errno.h>
+# include <string.h>
+# include "get_next_line.h"
+# include "../minilibx/mlx.h"
+
+typedef struct
+
+#endif
