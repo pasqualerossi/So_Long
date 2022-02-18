b82d6fd1c3881a0987e63f25cb7e1e80f01b683b
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-02-18 19:59:15 +1030

    Add files via upload

--- headers/so_long.h
+++ headers/so_long.h
@@ -8,6 +8,28 @@
 # include "get_next_line.h"
 # include "../minilibx/mlx.h"
 
-typedef struct
+typedef struct start
+{
+	int		fd;
+	int		heightmap;
+	int		widthmap;
+	int		playercount;
+	int		columncount;
+	int		exitcount;
+	int		playerfirst;
+	int		playersecond;
+	int		counter;
+
+	char	**map;
+
+	void	
+
+}	complete;
+
+int		exit_game(complete *game);
+void	check_errors(complete *game);
+int		map_reading(complete *game);
+int		controls_working(int command, complete *game);
+
 
 #endif
